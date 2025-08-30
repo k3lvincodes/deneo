d47cabad7e314168ef9ff6529999e21be9f6aec1
Kelvin Feh <k3lvincodes@gmail.com>
2025-08-30 08:48:19 +0000

    There's no water wave in the background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,7 +97,6 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'wave': 'wave 10s infinite linear',
       },
     },
   },
